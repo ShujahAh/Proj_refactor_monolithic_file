42123cf66e5dd936e952dc95367fe11847cfb20c
Ahmad Shujah <ahmad.shujah.18@cnu.edu>
2020-09-24 18:30:36 -0400

    finished

---
+++ fileio/fileio.cpp
@@ -0,0 +1,15 @@
+
+
+#include "../includes/fileio.h" 
+
+int loadData(const std::string filename, std::vector<process> &myProcesses){
+	return UNIMPLEMENTED; 
+}
+
+int saveData(const std::string filename,std::vector<process> &myProcesses){
+	return UNIMPLEMENTED;
+}
+
+
+
+
---
+++ utils/utils.cpp
@@ -0,0 +1,20 @@
+
+#include "../includes/utils.h"
+#include <vector>
+
+void sortData(const SORT_ORDER &mySortOrder ,std::vector<process> &myProcesses){
+
+}
+
+process getNext(std::vector<process> &myProcesses){
+	process p;
+	return p;
+}
+
+int getSize(std::vector<process> &myProcesses){
+	return UNIMPLEMENTED;
+}
+
+int handleMissingData(std::vector<process> &myProcesses){
+	return NO_DATA_TO_WORK_ON;
+}
